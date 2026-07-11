7d50f464dfef7887019e668fed0acceea7b911d3
VPMemoriaOS <vpu.muk@gmail.com>
2026-07-12 00:44:27 +0200

    Add missing repository imports to `__init__.py`

--- memoriaos/repository/__init__.py
+++ memoriaos/repository/__init__.py
@@ -1,5 +1,11 @@
+from .artifact_repository import ArtifactRepository
 from .observation_repository import ObservationRepository
+from .evidence_repository import EvidenceRepository
+from .claim_repository import ClaimRepository
 
 __all__ = [
+    "ArtifactRepository",
     "ObservationRepository",
+    "EvidenceRepository",
+    "ClaimRepository",
 ]
